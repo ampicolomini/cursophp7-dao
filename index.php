638325d392428c6c78dd9cf444d9fac5ad4c7646
Ana Maria Picolomin <ana.picolomini@gmail.com>
2021-01-24 18:06:07 -0300

    criamos a classe usuario

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 require_once("config.php");
-$sql = new SqL();
-$usuarios = $sql->select("select * from tb_usuarios");
-echo json_encode($usuarios);
-
+//$sql = new SqL();
+//$usuarios = $sql->select("select * from tb_usuarios");
+//echo json_encode($usuarios);
+$root = new Usuario();
+$root->loadById(6);
+echo $root;
 ?>
